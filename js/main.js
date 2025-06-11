9b1fc191f5b0a98b9be719f006151aae35dd6c76
Marat Aubakirov <marataub.511@gmail.com>
2025-06-11 21:21:12 +0300

    accordeon

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-const swiper = new Swiper(".swiper", {
+const swiperTop = new Swiper(".top__swiper", {
   // Optional parameters
-  loop: true,
+  effect: "fade",
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.querySelectorAll(".accordeon__triger").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.parentNode.classList.toggle("accordeon__item--active");
+  });
 });
